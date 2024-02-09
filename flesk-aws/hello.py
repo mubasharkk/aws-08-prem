c37dadf33f45e3092e62b0bdef94eafe9ecae173
Mubashar Khokhar <mubashar.khokhar@gastfreund.net>
2024-02-09 21:58:07 +0100

    Product detailed page aded

--- flesk-aws/hello.py
+++ flesk-aws/hello.py
@@ -12,6 +12,10 @@ def hello_world():
 def get_products():
     return "<p>Product page</p>"
 
+@app.route("products/<int:product_id>")
+def get_product(product_id):
+    return "<h1>Product detailed page for {{ product_id }}</h1>"
+
 if __name__ == "__main__":
     # app.run(debug=True)
     app.run(host="0.0.0.0", port=80)
